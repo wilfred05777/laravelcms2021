ae0dd6fe42b1613c8e3b7123beef2c4473d3003c
Wilfred <wilfred05777@gmail.com>
2021-02-22 16:16:23 +0800

    pages controller and index view

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [PagesController::class, 'index']);
 
 Route::get('/about', function (){
     return view('pages/about');
