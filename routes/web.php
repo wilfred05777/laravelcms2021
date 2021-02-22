6103fe3979d518376b7f0d8193b71b364cebda4c
Wilfred <wilfred05777@gmail.com>
2021-02-22 15:07:32 +0800

    install breeze and migration

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function (){
-//    return view('about');
-    return ("this is about route");
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
